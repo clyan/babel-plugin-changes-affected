d23fabc5f8c100c86611aa60e50933ef5b04f3b9
clayn <2429335889@qq.com>
2024-07-16 01:08:16 +0800

    fix: 修复import 引入 cjs模块导致打包错误的问题

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,30 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => {
   return {
     entry: ['src/index.ts'],
     outDir: './lib',
-    format: ['cjs', 'esm', 'iife'],
+    format: ['cjs'],
     dts: true,
     clean: true,
-    sourcemap: true,
+    sourcemap: !!options.watch,
     minify: !options.watch,
-    legacyOutput: true,
+    splitting: true,
+    cjsInterop: true,
+    // 如果使用 import 语句引入了cjs模块，同时cjs模块导出的是module.exports.default, 那么可以通过如下方式纠正导出解决
+    // https://github.com/evanw/esbuild/issues/532#issuecomment-1028893869
+    esbuildPlugins: [
+      {
+        // name 可以随意命名
+        name: 'modules-exports-default-parser',
+        setup(build) {
+          build.onResolve({ filter: /^@babel\/traverse$/ }, ({ kind }) => {
+            if (kind === 'import-statement')
+              return { path: resolve(__dirname, './esbuild/plugin/babel-traverse-wrapper.js') }
+          })
+        },
+      },
+    ],
   }
 })
